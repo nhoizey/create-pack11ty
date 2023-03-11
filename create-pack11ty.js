9150926fa1c907134890df40a60a11a0ca2a9f41
Nicolas Hoizey <nicolas@hoizey.com>
2023-03-12 00:57:28 +0100

    feat: use colors to style console messages

--- create-pack11ty.js
+++ create-pack11ty.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 
+const chalk = require("chalk");
+
 const currentNodeVersion = process.versions.node;
 const majorNodeVersion = currentNodeVersion.split(".")[0];
+const minimumNodeVersion = 16;
 
-if (majorNodeVersion < 16) {
-  console.error(`You are running Node ${currentNodeVersion}.
-"create pack11ty" requires Node 18 or higher.
-Please update your version of Node.`);
+if (majorNodeVersion < minimumNodeVersion) {
+  console.error(
+    `You are running Node ${currentNodeVersion}.
+${chalk.red(`"create pack11ty" requires Node ${minimumNodeVersion} or higher.`)}
+Please update your version of Node.`
+  );
   process.exit(1);
 }
 
@@ -16,25 +21,37 @@ const runCommand = (command) => {
   try {
     execSync(`${command}`, { stdio: "inherit" });
   } catch (e) {
-    console.error(`Failed to execute ${command}`, e);
+    console.error(chalk.red(`Failed to execute ${command}`), e);
     return false;
   }
   return true;
 };
 
 const repoName = process.argv[2];
+
+console.log(chalk.blue(`Cloning the repository with name ${repoName}`));
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/nhoizey/pack11ty ${repoName}`;
+try {
+  execSync(`${gitCheckoutCommand}`, { stdio: "inherit" });
+} catch (e) {
+  console.error(chalk.red(`Failed to check out pack11ty`), e);
+  process.exit(-1);
+}
+
+console.log(chalk.blue(`Installing dependencies for ${repoName}`));
 const installDepsCommand = `cd ${repoName} && npm install`;
-
-console.log(`Cloning the repository with name ${repoName}`);
-const checkedOut = runCommand(gitCheckoutCommand);
-if (!checkedOut) process.exit(-1);
-
-console.log(`Installing dependencies for ${repoName}`);
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) process.exit(-1);
+try {
+  execSync(`${installDepsCommand}`, { stdio: "inherit" });
+} catch (e) {
+  console.error(chalk.red(`Failed to install dependencies`), e);
+  process.exit(-1);
+}
 
 console.log(
-  "Congratulations! You are ready. Follow the following commands to start"
+  `
+${chalk.green(`Congratulations, you are ready!`)}
+
+${chalk.blue(`Execute the following commands to start:`)}
+cd ${repoName}
+npm start`
 );
-console.log(`cd ${repoName} && npm start`);
